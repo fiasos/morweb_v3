5a1a9d31a092904afe043d73b6932ae1e567613c
Brioche23 <alessio.brioche@gmail.com>
2022-01-31 23:38:17 +0100

    Fixed Globe

--- public/03_globe/js/sphere.js
+++ public/03_globe/js/sphere.js
@@ -1,14 +1,12 @@
 //  Standard global variables
 let trees = [];
 let pointLoaded = false;
-let container;
+let container, canvas;
 let camera, scene, raycaster, renderer;
-let INTERSECTED;
+let INTERSECTED = "";
 let sphere;
 const mouse = new THREE.Vector2();
 raycaster = new THREE.Raycaster();
-//raycaster.near = 10;
-//raycaster.far = 1000;
 
 init();
 animate();
@@ -18,15 +16,18 @@ animate();
 // title.innerHTML += "Discover our trees";
 
 function init() {
+  const w = window.innerWidth;
+  const h = window.innerHeight;
+
   container = document.createElement("div");
   document.body.appendChild(container);
   //  CAMERA
-  camera = new THREE.PerspectiveCamera(
-    20,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
+  //  *Funziona con entrambe le camere
+  camera = new THREE.PerspectiveCamera(20, w / h, 0.1, 1000);
+
+  // camera = new THREE.OrthographicCamera(w / -2, w / 2, h / 2, h / -2, 0, 10);
+  // camera.zoom = 100;
+
   camera.position.x = -10;
 
   //  SCENA
@@ -39,7 +40,7 @@ function init() {
     antialias: true,
     canvas: vascan,
   });
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(w, h);
   renderer.setClearColor(0xffffff, 0);
 
   //  GLOBO
@@ -56,10 +57,11 @@ function init() {
 
   //  ORBIT CONTROL
   controls = new THREE.OrbitControls(camera, renderer.domElement);
+  console.log(" renderer.domElement:", renderer.domElement);
   controls.minDistance = 7;
   controls.maxDistance = 20;
   controls.enableDamping = true;
-  controls.autoRotate = true;
+  controls.autoRotate = false;
   controls.autoRotateSpeed = 3;
   container.appendChild(renderer.domElement);
 
@@ -71,6 +73,8 @@ let textures = [];
 let materials = [];
 
 function addPoints() {
+  const size = 0.05;
+  const segments = 20;
   for (let i = 0; i < trees.length; i++) {
     let start = calc(trees[i].lat, trees[i].lng);
     textures[i] = new THREE.TextureLoader().load(
@@ -78,7 +82,7 @@ function addPoints() {
     );
     materials[i] = new THREE.MeshBasicMaterial({ map: textures[i] });
     let mesh = new THREE.Mesh(
-      new THREE.SphereGeometry(0.05, 20, 20),
+      new THREE.SphereGeometry(size, segments, segments),
       materials[i]
     );
     mesh.name = i;
@@ -95,6 +99,17 @@ function onMouseMove(event) {
   //  Calculate mouse position in normalized device coordinates (-1 to +1) for both components
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  // scene.add(
+  //   new THREE.ArrowHelper(
+  //     raycaster.ray.direction,
+  //     raycaster.ray.origin,
+  //     10,
+  //     0xff0000,
+  //     0.05,
+  //     0.05
+  //   )
+  // );
 }
 
 function animate() {
@@ -105,6 +120,7 @@ function animate() {
     addPoints();
     pointLoaded = true;
   }
+  //  TODO Aggiungere loading prima di punti
   render();
 }
 
@@ -122,26 +138,30 @@ function render() {
   camera.updateMatrixWorld();
   //   update the picking ray with the camera and mouse position
   raycaster.setFromCamera(mouse, camera);
+
   //   calculate objects intersecting the picking ray
   const intersects = raycaster.intersectObjects(sphere.children, false);
-  // const dai = raycaster.intersectObjects(sphere, false);
 
   if (intersects.length > 0) {
-    if (INTERSECTED != intersects[0].object) {
-      INTERSECTED = intersects[0].object;
-      //  Scale Up
-      INTERSECTED.scale.x = INTERSECTED.scale.y = INTERSECTED.scale.z = 1.4;
+    // console.log("intersects:", intersects);
+    intersects.forEach((i) => {
+      if (INTERSECTED != i.object) {
+        INTERSECTED = i.object;
+        //  Scale Up
+        INTERSECTED.scale.x = INTERSECTED.scale.y = INTERSECTED.scale.z = 1.4;
 
-      //  Nome dell'albero
-      let treeIndex = INTERSECTED.name;
-      document.getElementById("treeName").innerHTML = trees[treeIndex].type;
-      //  Numero di strappi
-      let treeTear = INTERSECTED.tear;
-      document.getElementById("treeStrappi").innerHTML =
-        "Sheets:  " + trees[treeTear].strappi;
-      console.log(trees[treeIndex].strappi);
-      console.log(trees[treeTear].type);
-    }
+        //  Nome dell'albero
+        let treeIndex = INTERSECTED.name;
+        document.getElementById("treeName").innerHTML = trees[treeIndex].type;
+        //  Numero di strappi
+        let treeTear = INTERSECTED.tear;
+        document.getElementById(
+          "treeStrappi"
+        ).innerHTML = `Sheets: ${trees[treeTear].strappi}`;
+        console.log(trees[treeIndex].strappi);
+        console.log(trees[treeTear].type);
+      }
+    });
   } else {
     if (INTERSECTED)
       //  Scale down
@@ -152,4 +172,13 @@ function render() {
 
   controls.update();
   renderer.render(scene, camera);
+}
+
+window.addEventListener("resize", onWindowResize, false);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
