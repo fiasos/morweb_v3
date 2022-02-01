b3c03cb66c4453ae3220bbcf53b1f99584995387
Brioche23 <alessio.brioche@gmail.com>
2022-02-01 18:55:11 +0100

    Semplified DB

--- public/04_shop/info_albero.js
+++ public/04_shop/info_albero.js
@@ -7,12 +7,17 @@ let currentSlide = 0;
 let immagini = [];
 
 let titolo = document.getElementById("titolo");
-let slideshow = document.getElementById("slides");
-let info = document.getElementById("info");
+let provenienza = document.getElementById("provenienza");
+let slide = document.getElementById("slide");
+let prezzo_span = document.getElementById("prezzo");
+let titoletto = document.getElementById("titolett");
+let forest = document.getElementById("forest");
+let lat = document.getElementById("lat");
+let lng = document.getElementById("lng");
 let descrizione = document.getElementById("descrizione");
-let features = document.getElementById("features");
-let banner = document.getElementById("banner_testo").parentNode;
-let banner_testo = document.getElementById("banner_testo");
+let foto = document.getElementById("foto");
+let dx = document.getElementById("dx");
+let sx = document.getElementById("sx");
 let link_banner = document.getElementById("link_banner");
 
 function loadInfo() {
@@ -38,73 +43,34 @@ function loadInfo() {
     }
 
     //  Titolo
-    titolo.innerHTML = `<h1 class="font-cronos font-bold md:text-6xl xxs:text-5xl">${tree.type}</h1> 
-      <h2 class="font-cronos font-semibold md:text-4xl xxs:text-3xl">${tree.provenienza}</h2>`;
+    titolo.innerHTML = tree.type;
+    provenienza.innerHTML = tree.provenienza;
 
     //  Slideshow
     const URL_img = `/public/04_shop/01_img/02_slideshow/${tree.id}`;
-    slideshow.innerHTML = `<img id="slide" src="${URL_img}/a.jpg" style="width: 100%">`;
+    slide.setAttribute("src", ` ${URL_img}/a.jpg`);
     immagini = [`${URL_img}/a.jpg`, `${URL_img}/b.jpg`, `${URL_img}/c.jpg`];
-    const div = document.createElement("div");
-    div.setAttribute("class", "card");
 
-    //  Info
-    info.innerHTML = `<span class="block text-xl font-medium leading-5">€ ${prezzo}</span>
-    <span class="block text-lg font-light">per sheet</span>
-    <div class="my-6">
-        <a href="/public/06_project/project.html" class="button">Subscribe</a>
-    </div>`;
+    //  prezzo
+    prezzo_span.innerHTML = `€ ${prezzo}`;
 
     //  Descrizione
-    descrizione.innerHTML = `<div class="py-8">
-    <div class="flex flex-row justify-between"> 
-       <div>
-          <span class="titoletto">${tree.type}</span>
-             <a href="/public/03_globe/globe.html">
-              <ul class="py-3">
-                <li class="hover:underline">Ancient Forest,${tree.provenienza}</li> 
-                <li>${tree.lat}</li>
-                <li>${tree.lng}</li>
-              </ul> 
-          </a>
-          <span class="paragrafo">${tree.descrizione}</span>
-          <div class="pt-16">
-                <span class="titoletto">Scientific classification</span>
-                <div class="py-3 flex flex-row sm:flex-wrap">
-                  <ul class="font-medium pr-14 ">
-                   <li>Bionomial name</li>
-                   <li>Order</li> 
-                   <li>Family</li>
-                   <li>Genus</li>
-                  </ul> 
-                  <ul>
-                    <li>Acer saccharum</li>
-                    <li>Sapindales</li>
-                    <li>Sapindaceae</li>
-                    <li>Acer</li>
-                  </ul>
-                </div>
-          </div>                           
-       </div>
-        <img class="w-auto h-140 pl-20 xxs:hidden md:inline-block" src="01_img/03_trees/${tree.id}.jpg">
-    </div>
-  </div>`;
+    titoletto.innerHTML = tree.type;
+    forest.innerHTML = `Ancient Forest, ${tree.provenienza}`;
+    lat.innerHTML = tree.lat;
+    lng.innerHTML = tree.lng;
+    descrizione.innerHTML = tree.descrizione;
+    foto.setAttribute("src", `01_img/03_trees/${tree.id}.jpg `);
 
     //   features.innerHTML = "BellaAA"
 
     const URL_texture = `/public/04_shop/01_img/04_texture/${tree.id}`;
-    const img_sx = document.createElement("img");
-    img_sx.setAttribute("src", `${URL_texture}/sx.svg`);
-    img_sx.setAttribute("alt", `${tree.type}`);
-    img_sx.setAttribute("class", `xxs:hidden sm:inline-block`);
-    const imd_dx = document.createElement("img");
-    imd_dx.setAttribute("src", `${URL_texture}/dx.svg`);
-    imd_dx.setAttribute("alt", `${tree.type}`);
-    imd_dx.setAttribute("class", `xxs:hidden sm:inline-block`);
+    sx.setAttribute("src", `${URL_texture}/sx.svg`);
+    sx.setAttribute("alt", `${tree.type}`);
+    dx.setAttribute("src", `${URL_texture}/dx.svg`);
+    dx.setAttribute("alt", `${tree.type}`);
 
-    banner.insertBefore(img_sx, banner_testo);
-    banner_testo.after(imd_dx);
-
+    //! Non cambia colore
     link_banner.setAttribute(
       "class",
       `text-${colore} font-thin md:text-lg sm:text-base xxs:text-lg hover:underline`
@@ -122,6 +88,7 @@ async function asyncCall() {
 
 asyncCall();
 
+//  Per slideshow
 function prevSlide() {
   currentSlide = currentSlide - 1;
   if (currentSlide === -1) {
@@ -140,13 +107,6 @@ function nextSlide() {
 
 function showSlide() {
   let imageFile = immagini[currentSlide];
-
-  //recupero il valore specificato in html dall'id 'slide' (ovvero il contenitore dove vengono posizionate le immagini)
   let imgTag = document.getElementById("slide");
-  //assegno a imgTag.src il valore 'imageFile', ovvero il nome dell'immagine da mostare
   imgTag.src = imageFile;
-}
-
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
 }
